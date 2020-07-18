c968e7d3b57b1976f8fa6fa22196ab7f1f018a89
Eddie Antonio Santos <easantos@ualberta.ca>
2020-07-18 09:14:38 -0600

    refactor: introduce fenceposts

--- word_boundary/__init__.py
+++ word_boundary/__init__.py
@@ -1,8 +1,25 @@
+from enum import Enum
+
+
+class Op(Enum):
+    UNASSIGNED = "?"
+    BOUNDARY = "÷"
+    NO_BOUNDARY = "×"
+
+
 def word_boundaries(text: str):
     if not text:
         return
 
+    fenceposts = [Op.UNASSIGNED] * (len(text) + 1)
+    assert len(fenceposts) >= 2
+
     # WB1
-    yield 0
+    fenceposts[0] = Op.BOUNDARY
+
     # WB2
-    yield len(text)
+    fenceposts[-1] = Op.BOUNDARY
+
+    for index, op in enumerate(fenceposts):
+        if op == Op.BOUNDARY:
+            yield index
