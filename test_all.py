0241b242443afca6829b823bc830291ce9074ea2
Eddie Antonio Santos <easantos@ualberta.ca>
2020-07-18 10:50:20 -0600

    test: newline rule

--- test_all.py
+++ test_all.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-from hypothesis import given
-from hypothesis.strategies import text
+from hypothesis import given, assume
+from hypothesis.strategies import text, sampled_from
 
 from word_boundary import word_boundaries
 from custom_strategies import ideographs, non_empty_text
@@ -23,10 +23,31 @@ def test_sot_eot(example: str) -> None:
     assert boundaries[-1] == len(example)
 
 
-def test_crlf():
+def test_crlf() -> None:
     # TODO: come up with a more robust test case
     boundaries = list(word_boundaries("\r\n"))
     assert boundaries == [0, len("\r\n")]
+
+
+@given(
+    sampled_from(
+        "\r"
+        "\n"
+        "\N{LINE TABULATION}"
+        "\N{FORM FEED}"
+        "\N{NEXT LINE}"
+        "\N{LINE SEPARATOR}"
+        "\N{PARAGRAPH SEPARATOR}"
+    ),
+    non_empty_text(),
+    non_empty_text(),
+)
+def test_newline(newline: str, left: str, right: str) -> None:
+    assume(not (left.endswith("\r") and newline == "\n"))
+    left_boundaries = len(list(word_boundaries(left)))
+    right_boundaries = len(list(word_boundaries(right)))
+    example = left + newline + right
+    assert len(list(word_boundaries(example))) == left_boundaries + right_boundaries
 
 
 @given(ideographs())
