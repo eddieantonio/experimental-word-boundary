8c4e711ca2989bb3c2d780aacf9b41f5e49d2a24
Eddie Antonio Santos <easantos@ualberta.ca>
2020-07-18 09:33:09 -0600

    test: break on ideographs

--- test_all.py
+++ test_all.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 from hypothesis import given
-from hypothesis.strategies import text
+from hypothesis.strategies import text, characters
 
 from word_boundary import word_boundaries
 
@@ -22,7 +22,26 @@ def test_sot_eot(example: str) -> None:
     assert boundaries[-1] == len(example)
 
 
+def ideographs():
+    START_OF_CJK_BLOCK = 0x4E00
+    END_OF_CJK_BLOCK = 0x9FFF
+    return text(
+        min_size=1,
+        alphabet=characters(
+            whitelist_categories=("Lo",),
+            min_codepoint=START_OF_CJK_BLOCK,
+            max_codepoint=END_OF_CJK_BLOCK,
+        ),
+    )
+
+
 def test_crlf():
     # TODO: come up with a more robust test case
     boundaries = list(word_boundaries("\r\n"))
     assert boundaries == [0, len("\r\n")]
+
+
+@given(ideographs())
+def test_break_everywhere(ideographs: str) -> None:
+    # There should be as many boundaries as possible
+    assert len(list(word_boundaries(ideographs))) == len(ideographs) + 1
